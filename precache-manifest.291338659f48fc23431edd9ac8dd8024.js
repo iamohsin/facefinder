995d875e7a729c0c4a49dad2620339b17f52ccc9
iamohsin <59395132+iamohsin@users.noreply.github.com>
2020-01-30 19:34:07 +0500

    Updates

---
+++ precache-manifest.291338659f48fc23431edd9ac8dd8024.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a514df64fb44b723e70add21782fce05",
+    "url": "/facefinder/index.html"
+  },
+  {
+    "revision": "d39eb3e96a0ac4df3a99",
+    "url": "/facefinder/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7a0b01445d9246981c7d",
+    "url": "/facefinder/static/css/main.5e41a273.chunk.css"
+  },
+  {
+    "revision": "d39eb3e96a0ac4df3a99",
+    "url": "/facefinder/static/js/2.23cb24e7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/facefinder/static/js/2.23cb24e7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a0b01445d9246981c7d",
+    "url": "/facefinder/static/js/main.3f2dfca9.chunk.js"
+  },
+  {
+    "revision": "cb27efff2095d7f51cc1",
+    "url": "/facefinder/static/js/runtime-main.cee4f7ed.js"
+  }
+]);
